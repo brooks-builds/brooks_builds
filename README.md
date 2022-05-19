e78755be19a29dea3a85e0114737f6bfb64ebd92
Brookzerker <brooks@brookzerker.com>
2022-05-19 08:35:23 -0600

    WIP - using pulumi to set up cloudfront

--- README.md
+++ README.md
@@ -8,7 +8,14 @@ I'm going to be doing things slightly differently as I'll be skipping having Pul
 
 The general idea is
 
-- [ ] Create the S3 bucket
-- [ ] Build the project for release using `trunk`
-- [ ] Create the Cloudfront distribution
+- [x] Create the S3 buckets
+  - [x] Code
+  - [x] Logs
+- [x] Certificate
+  - [x] Get existing if exists in config
+  - [x] Create if not
 - [ ] Create a Route53 record
+- [ ] Create the Cloudfront distribution
+- [ ] Build the project for release using `trunk`
+
+left off at https://github.com/pulumi/examples/blob/2ee921d930a40e4be20ce68b12a1e00356888b5f/aws-ts-static-website/index.ts#L156
--- index.ts
+++ index.ts
@@ -1,15 +1,83 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import { BucketArgs } from "@pulumi/aws/s3";
+import { Certificate, CertificateValidation } from "@pulumi/aws/acm";
+import { Output } from "@pulumi/pulumi";
 
-function createBucket(): aws.s3.Bucket {
-  return new aws.s3.Bucket("Code S3 Bucket", {
-    bucketPrefix: "brooks_builds_platform_frontend",
-    website: {
+function createWebBucket(name: string, website: boolean = false): aws.s3.Bucket {
+  const options: BucketArgs = {
+    bucketPrefix: name,
+  };
+
+  if(website) {
+    options.website = {
       indexDocument: "index.html",
       errorDocument: "index.html",
-    }
-  })
-} 
+    };
+  }
 
-const bucket = createBucket();
+  return new aws.s3.Bucket(name, options);
+}
+
+function createCertificate(domainName: string): Certificate {
+  return new Certificate("Certificate", {
+    domainName,
+    validationMethod: "DNS",
+    subjectAlternativeNames: [`www.${domainName}`]
+  });
+}
+
+async function getRoute53HostedZoneId(domainName: string): Promise<string> {
+  const zone = await aws.route53.getZone({name: domainName});
+  return zone.zoneId
+}
+
+function createRoute53ValidationRecord(certificate: Certificate, validationOptionIndex: number, zoneId: string, ttl: number): aws.route53.Record {
+  if(!certificate.domainValidationOptions[validationOptionIndex]) throw new Error(`domain validation options doesn't have index ${validationOptionIndex}`);
+
+  return new aws.route53.Record("Validation Record", {
+    name: certificate.domainValidationOptions[validationOptionIndex].resourceRecordName,
+    type: certificate.domainValidationOptions[validationOptionIndex].resourceRecordType,
+    zoneId,
+    records: [certificate.domainValidationOptions[validationOptionIndex].resourceRecordValue],
+    ttl
+  })
+}
+
+function validateCertificate(certificate: Certificate, validationRecordFqdns: Output<string>[]): CertificateValidation {
+  return new CertificateValidation("Validate created certificate", {
+    certificateArn: certificate.arn,
+    validationRecordFqdns
+
+  })
+}
+
+async function main(): Promise<void> {
+  const config = new pulumi.Config();
+  let certificateArn = config.get("certificateArn");
+  const domainName = config.get("domainName");
+  
+  if(!domainName) throw new Error("missing config 'domainName'");
+  
+  const bucketName = `brooks_builds_platform_${pulumi.getStack()}`;
+  const logBucketName = `${bucketName}_logs`;
+  const bucket = createWebBucket(bucketName, true);
+  const logBucket = createWebBucket(logBucketName);
+  
+  if(!certificateArn) {
+    const certificate = createCertificate(domainName);
+    const hostedZoneId = await getRoute53HostedZoneId(domainName);
+    const timeToLiveInSeconds = 60 * 10;
+    const validationRecord = createRoute53ValidationRecord(certificate, 0, hostedZoneId, timeToLiveInSeconds);
+    const wwwValidationRecord = createRoute53ValidationRecord(certificate, 1, hostedZoneId, timeToLiveInSeconds);
+    const validationRecordFqdns = [
+      validationRecord.fqdn,
+      wwwValidationRecord.fqdn
+    ];
+    const validatedCertificate = validateCertificate(certificate, validationRecordFqdns);
+    certificateArn = validatedCertificate.certificateArn.get();
+  }
+}
+
+main().then(() => console.log("finished"));
