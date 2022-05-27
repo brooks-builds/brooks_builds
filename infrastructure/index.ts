5a91d0335548bcbc318837953e0d00e93a0d3594
Brookzerker <brooks@brookzerker.com>
2022-05-27 08:24:39 -0600

    WIP not able to create the EC2 instance yet

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,9 +1,64 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import { Ami, getAmi, SecurityGroup, Vpc } from "@pulumi/aws/ec2";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+function createEc2Instance(name: string, ami: aws.ec2.GetAmiResult, securityGroup: SecurityGroup, vpcId: string): aws.ec2.Instance {
+  return new aws.ec2.Instance(name, {
+    ami: ami.id,
+    associatePublicIpAddress: true,
+    vpcSecurityGroupIds: [
+      securityGroup.id,
+    ],
+    instanceType: 't2.micro',
+  });
+}
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+function getAwsAmi(): Promise<aws.ec2.GetAmiResult> {
+  return getAmi({
+    owners: ['amazon'],
+    filters: [{
+      name: 'image-id',
+      values: ['ami-0022f774911c1d690']
+    }],
+    mostRecent: true
+  });
+}
+
+function createSSHSecurityGroup(vpcId: string): SecurityGroup {
+  const name = 'Allow SSH';
+  return new SecurityGroup('SSH Security Group', {
+    description: 'Security group allowing SSH into a resource',
+    egress: [{
+      fromPort: 0,
+      protocol: 'ALL',
+      toPort: 0,
+      cidrBlocks: ['0.0.0.0/0'],
+      description: 'Allow everything everywhere',
+    }],
+    ingress: [{
+      fromPort: 22,
+      protocol: 'tcp',
+      toPort: 22,
+      cidrBlocks: ['0.0.0.0/0'],
+      description: 'Allow SSH from anywhere in!',
+    }],
+    name,
+    vpcId,
+    tags: {
+      Name: name
+    }
+  })
+}
+
+export async function main(): Promise<any> {
+  const config = new pulumi.Config();
+  const vpcId = config.require('vpcId');
+  const ami = await getAwsAmi();
+  const sshSecurityGroup = createSSHSecurityGroup(vpcId);
+  const ec2 = createEc2Instance("brooks-builds-ec2", ami, sshSecurityGroup);
+  return {
+  };
+}
+
+main()
