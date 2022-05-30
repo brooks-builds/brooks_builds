6a551c1209f5e190f0c7c242a3e27777e7c0580a
Brookzerker <brooks@brookzerker.com>
2022-05-30 09:27:09 -0600

    set up an ec2 instance, and seq to run on that ec2 instance

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -2,15 +2,18 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import { Ami, getAmi, SecurityGroup, Vpc } from "@pulumi/aws/ec2";
+import { Input, Output } from "@pulumi/pulumi";
+import * as fs from 'fs';
 
-function createEc2Instance(name: string, ami: aws.ec2.GetAmiResult, securityGroup: SecurityGroup, vpcId: string): aws.ec2.Instance {
+function createEc2Instance(name: string, ami: aws.ec2.GetAmiResult, securityGroupIds: Output<string>[], keyName: string, subnetId: string, userData: string): aws.ec2.Instance {
   return new aws.ec2.Instance(name, {
     ami: ami.id,
     associatePublicIpAddress: true,
-    vpcSecurityGroupIds: [
-      securityGroup.id,
-    ],
+    vpcSecurityGroupIds: securityGroupIds,
     instanceType: 't2.micro',
+    keyName,
+    subnetId,
+    userData
   });
 }
 
@@ -51,14 +54,79 @@ function createSSHSecurityGroup(vpcId: string): SecurityGroup {
   })
 }
 
+function createAdminGuiSecurityGroup(vpcId: string): SecurityGroup {
+  const name = 'Allow Admin access to web apps';
+  return new SecurityGroup('GUI Admin Security Group', {
+    description: 'Security group allowing Admin access',
+    egress: [{
+      fromPort: 0,
+      protocol: 'ALL',
+      toPort: 0,
+      cidrBlocks: ['0.0.0.0/0'],
+      description: 'Allow everything everywhere',
+    }],
+    ingress: [{
+      fromPort: 8000,
+      protocol: 'tcp',
+      toPort: 8000,
+      cidrBlocks: ['0.0.0.0/0'],
+      description: 'Allow access to SEQ from anywhere in!',
+    }],
+    name,
+    vpcId,
+    tags: {
+      Name: name
+    }
+  })
+}
+
+function createNormalUsageSecurityGroup(vpcId: string): SecurityGroup {
+  const name = 'Allow normal access to hosted services';
+  return new SecurityGroup('Usage Security Group', {
+    description: 'Security group allowing Normal usage',
+    egress: [{
+      fromPort: 0,
+      protocol: 'ALL',
+      toPort: 0,
+      cidrBlocks: ['0.0.0.0/0'],
+      description: 'Allow everything everywhere',
+    }],
+    ingress: [{
+      fromPort: 5341,
+      protocol: 'tcp',
+      toPort: 5341,
+      cidrBlocks: ['0.0.0.0/0'],
+      description: 'Allow access to SEQ injest from anywhere in!',
+    }],
+    name,
+    vpcId,
+    tags: {
+      Name: name
+    }
+  })
+}
+
+function getEc2UserdataScript(): string {
+  return fs.readFileSync("./userdata/ec2.sh", {encoding: 'utf8'});
+}
+
 export async function main(): Promise<any> {
   const config = new pulumi.Config();
   const vpcId = config.require('vpcId');
+  const keyName = config.require('keyName');
+  const subnetId = config.require('subnetId');
   const ami = await getAwsAmi();
   const sshSecurityGroup = createSSHSecurityGroup(vpcId);
-  const ec2 = createEc2Instance("brooks-builds-ec2", ami, sshSecurityGroup);
-  return {
-  };
+  const adminSecurityGroup = createAdminGuiSecurityGroup(vpcId);
+  const normalUsageSecurityGroup = createNormalUsageSecurityGroup(vpcId);
+  const securityGroupIds = [
+    // sshSecurityGroup.id,
+    adminSecurityGroup.id,
+    // normalUsageSecurityGroup.id,
+  ];
+  const ec2UserData = getEc2UserdataScript();
+  const ec2 = createEc2Instance("brooks-builds-ec2", ami, securityGroupIds, keyName, subnetId, ec2UserData);
+  return {};
 }
 
-main()
+export const output = main();
