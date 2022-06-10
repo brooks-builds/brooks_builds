087b88bcf09f715234c35c4ae2ca20b8930ac287
Brookzerker <brooks@brookzerker.com>
2022-06-10 08:28:02 -0600

    added tailwind into the project

--- platform/webapp/src/components/top_menu.rs
+++ platform/webapp/src/components/top_menu.rs
@@ -5,13 +5,19 @@ use yew::prelude::*;
 #[styled_component(TopMenu)]
 pub fn top_menu() -> Html {
     html! {
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <div class="navbar-brand">
+      <nav class="h-8">
+        <div class="grid grid-cols-3 gap-4 px-5">
+            <div class="flex items-center">
                 <a href="#">
-                    <img src="static/brooks.png" alt="Brooks Builds Icon" class={css!("height: 2rem;")} />
+                    <img src="static/brooks.png" alt="Brooks Builds Icon" class="h-8" />
                 </a>
                 {"Brooks Builds"}
+            </div>
+            <div class="flex justify-center">
+              {"I am in the middle"}
+            </div>
+            <div class="flex justify-end">
+              {"right side"}
             </div>
         </div>
       </nav>
