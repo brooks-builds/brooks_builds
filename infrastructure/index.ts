dadb29b5fbd1b978a7319f18f3e7497b3b9eb62b
Brookzerker <brooks@brookzerker.com>
2022-06-27 20:27:59 -0600

    starting over with Pulumi

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,24 +1,13 @@
-import * as pulumi from '@pulumi/pulumi';
-import BBAwsVpc from './aws/vpc';
-import {bbAwsEc2Init} from './aws/ec2';
-import BBAwsSecurityGroups from './aws/securityGroups';
-import BBAwsLoadBalancer from './aws/loadBalancer';
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 
-async function main(): Promise<any> {
-  const config = new pulumi.Config();
-  const bbAwsVpc = new BBAwsVpc(config.require('vpcId'), config.require('subnetId'));
-  const securityGroups = new BBAwsSecurityGroups(bbAwsVpc);
-  const bbAwsEc2 = await bbAwsEc2Init(
-    config.require('keyName'), 
-    './userdata/ec2.sh',
-    bbAwsVpc,
-    securityGroups,
-  );
-  const loadBalancer = new BBAwsLoadBalancer(securityGroups, bbAwsVpc);
+const stack = pulumi.getStack();
 
-  return {
-    ami: bbAwsEc2.ami?.id
-  }
-}
-
-export const output = main();
+const platformFrontendBucket = new aws.s3.Bucket("platformWebBucket", {
+    bucketPrefix: `platform-frontend-${stack}`,
+    tags: {
+        project: `platform`,
+        stack
+    }
+})
